3e31c4a9a795847e4da736a15c2c8c51fa55b3ee
geosau <geoyurchenko2004@gmail.com>
2025-04-08 03:35:51 +0300

    c to cpp

---
+++ hashmap.cpp
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "hashmap.h"
+
+#include <stdio.h>
+
+const size_t rehash_move = 128;
+
+
+
+
---
+++ hashmap.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+struct DictNode {
+    uint64_t hashVal;
+    DictNode *next;
+};
+
+struct Dict {
+    DictNode **table;
+    size_t mask;
+    size_t size; 
+};
+
+
+struct HashMap {
+    Dict *oldDict;
+    Dict *newDict;
+    size_t curr_migrate;
+};
